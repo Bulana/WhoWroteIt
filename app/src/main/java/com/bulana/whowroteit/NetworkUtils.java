8a9657b6e07f7d0b954a42beb1c86c88a7e8e496
Bulana <Bulana@hal9000.zapper.local>
2019-06-07 12:16:18 +0200

    NetworkUtils class created

--- app/src/main/java/com/bulana/whowroteit/NetworkUtils.java
+++ app/src/main/java/com/bulana/whowroteit/NetworkUtils.java
@@ -1,4 +1,41 @@
 package com.bulana.whowroteit;
 
+import android.net.Uri;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class NetworkUtils {
+    private static final String LOG_TAG = NetworkUtils.class.getSimpleName();
+
+    private static final String BOOK_BASE_URL = "https://www.googleapis.com/books/v1/volumes?";
+    private static final String QUERY_PARAM = "q";
+    private static final String MAX_RESULTS = "maxResults";
+    private static final String PRINT_TYPE = "printType";
+
+    static String getBookInfo(String queryString) {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String bookJSONString = null;
+
+        try {
+
+            Uri buildURI = Uri.parse(BOOK_BASE_URL).buildUpon()
+                    .appendQueryParameter(QUERY_PARAM, queryString)
+                    .appendQueryParameter(MAX_RESULTS, "10")
+                    .appendQueryParameter(PRINT_TYPE, "books")
+                    .build();
+
+            URL requestURL = new URL(buildURI.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+
+        return bookJSONString;
+    }
 }
